7222675127d605c22e6672c7493dc4093ef5e858
taylornorris <taylornorrisdev@gmail.com>
2024-06-08 09:16:41 -0600

    Complete project requirements

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const posts = [
     {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
+        name: "Vincent van Gogh", //
+        username: "vincey1853", //
+        location: "Zundert, Netherlands", //
+        avatar: "images/avatar-vangogh.jpg", //
+        post: "images/post-vangogh.jpg", //
+        comment: "just took a few mushrooms lol", //
         likes: 21
     },
     {
