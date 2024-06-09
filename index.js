3d32e91fe5defe6ff29be98b3d356b88d249ac3a
taylornorris <taylornorrisdev@gmail.com>
2024-06-09 08:43:11 -0600

    Refactor html and css

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+// provided with project material
 const posts = [
     {
-        name: "Vincent van Gogh", //
-        username: "vincey1853", //
-        location: "Zundert, Netherlands", //
-        avatar: "images/avatar-vangogh.jpg", //
-        post: "images/post-vangogh.jpg", //
-        comment: "just took a few mushrooms lol", //
+        name: "Vincent van Gogh", 
+        username: "vincey1853", 
+        location: "Zundert, Netherlands", 
+        avatar: "images/avatar-vangogh.jpg", 
+        post: "images/post-vangogh.jpg", 
+        comment: "just took a few mushrooms lol", 
         likes: 21
     },
     {
